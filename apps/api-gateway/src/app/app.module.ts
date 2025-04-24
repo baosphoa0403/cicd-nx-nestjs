57c7124857baf48e701ebb3b7d481aa2b4a87e91
baosphoa0403 <baosphoa0403@gmail.com>
2025-04-24 15:29:02 +0700

    first commit

---
+++ apps/api-gateway/src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { GlobalModule } from '@cicd-nx/global';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [GlobalModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
